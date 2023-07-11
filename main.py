8acaa2f1b608e9e15302e79537ab62cff1f33ffe
alexalaver <alex.alaverdyan.04@gmail.com>
2023-07-12 03:54:00 +0400

    dat

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ logging.basicConfig(level=logging.INFO)
 
 bot = Bot(cfg.token)
 dp = Dispatcher(bot, storage=MemoryStorage())
-db = Data('95.216.65.93', '14369', 'mando_data', 'mando_user', 'mando443322')
+db = Data('192.168.2.20', '5432', 'mando_data', 'mando_user', 'mando443322')
 
 class register_column(StatesGroup):
     text = State()
