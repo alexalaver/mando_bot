95399eedbe3f73b8ba69d3830f834e54b69254d2
alexalaver <alex.alaverdyan.04@gmail.com>
2023-07-13 16:41:23 +0400

    main

--- main.py
+++ main.py
@@ -388,7 +388,7 @@ async def settings(message: types.Message, state: FSMContext):
             markup_inline.add(button1, button2, button3)
             await message.answer('test', reply_markup=markups)
             await message.answer(db.get_texts(user_id, 'change_language_text'), reply_markup=markup_inline)
-            await state.finish()
+            await settings_user.settings_lang.set()
         elif message.text == back:
             markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
             button1 = types.KeyboardButton(db.get_texts(user_id, 'settings'))
